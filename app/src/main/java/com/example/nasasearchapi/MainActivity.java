2a1aac23dedc44119343711a9cd838c3140016e5
brady <bradyhuai@outlook.com>
2023-06-08 12:13:20 -0400

    Add search view to main page

--- app/src/main/java/com/example/nasasearchapi/MainActivity.java
+++ app/src/main/java/com/example/nasasearchapi/MainActivity.java
@@ -3,12 +3,25 @@ package com.example.nasasearchapi;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.ListView;
+import android.widget.SearchView;
 
 public class MainActivity extends AppCompatActivity {
+
+    // UI Components
+    SearchView searchView;
+    ListView contentList;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        initUIComponents();
+    }
+
+    private void initUIComponents(){
+        searchView = findViewById(R.id.search_view);
+        contentList = findViewById(R.id.content_list);
     }
 }
