697d9235854047f313b5871890bbbd495f548e00
brady <bradyhuai@outlook.com>
2023-06-09 13:54:50 -0400

    Add constants

---
+++ app/src/main/java/com/example/nasasearchapi/tools/Constants.java
@@ -0,0 +1,8 @@
+package com.example.nasasearchapi.tools;
+
+public class Constants {
+    public static final String API_ROOT = "https://images-api.nasa.gov";
+    public static final String SEARCH_ENDPOINT = "/search";
+    public static final String ASSET_ENDPOINT = "/asset";
+
+}
